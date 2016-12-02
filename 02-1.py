c33f761bc9eb7f21ed0e2d2efe30438488eefdc2
Michael Wagner <Michael_Wagner@mentor.com>
2016-12-02 17:50:43 +0100

    second day second riddle

--- 02-1.py
+++ 02-1.py
@@ -6,14 +6,17 @@ parser = argparse.ArgumentParser(description='Find pin to toilet')
 parser.add_argument('input', metavar='file', type=argparse.FileType('r'),
                     help='file directions on keypad starting on 5')
 
-keypad = ['1','2','3','4','5','6','7','8','9']
-pos = [0,0]
+keypad = ['1','2','3','4','5','6','7','8','9','A','B','C','D']
+shiftval = [2,5,6,7,10]
+pos = [0,-2]
 
 def addDirection(pos, dir):
     def addElement(index,value):
+        global pos
         tmp = list(pos)
         tmp[index] = pos[index]+value
-        if abs(tmp[index]) <= 1: pos[index]=tmp[index]
+        if sum([abs(x) for x in tmp]) <= 2:
+            pos = tmp
 
     if dir in ['U','D']:
         addElement(0,-1 if dir == 'U' else 1)
@@ -21,7 +24,13 @@ def addDirection(pos, dir):
         addElement(1,-1 if dir == 'L' else 1)
 
 def getKey(pos):
-    return keypad[ (pos[0]+1)*3 + pos[1] + 1 ]
+    def computeIndex(pos):
+        i = [x+2 for x in pos]
+        index = i[0]*5 + i[1]
+        shift = shiftval[i[0]]
+        return index - shift
+
+    return keypad[ computeIndex(pos) ]
 
 args = parser.parse_args()
 
