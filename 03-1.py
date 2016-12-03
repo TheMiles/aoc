87e2912f981b0b37f9fc14145024b96b92484f40
Miles <miles@coders-of-the-caribbean.org>
2016-12-04 00:05:55 +0100

    some refactoring

--- 03-1.py
+++ 03-1.py
@@ -2,21 +2,28 @@
 
 import argparse
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=argparse.FileType('r'),
                     help='the input file')
 
 args = parser.parse_args()
 
+
+def is_possible_triangle(l):
+	l.sort()
+	return sum(l[:-1]) > l[-1]
+
+
+
 count_good = 0
 count_total = 0
 
 for line in args.file.readlines():
-	lengths = [ int(x) for x in line.strip().split()]
-	lengths.sort()
-	if len(lengths) != 3:
-		raise ValueError('Wron number of entries in a line')
-	if sum(lengths[:-1]) > lengths[-1]:
+	triangle = [ int(x) for x in line.strip().split()]
+	if len(triangle) != 3:
+		raise ValueError('Wrong number of entries in a line')
+	if is_possible_triangle(triangle):
 		count_good += 1
 	count_total += 1
 
