e65895f1fe779c9b7f0fb08339fa8fb398782f1b
Miles <miles@coders-of-the-caribbean.org>
2016-12-04 00:01:52 +0100

    Third day - first problem

---
+++ 03-1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=argparse.FileType('r'),
+                    help='the input file')
+
+args = parser.parse_args()
+
+count_good = 0
+count_total = 0
+
+for line in args.file.readlines():
+	lengths = [ int(x) for x in line.strip().split()]
+	lengths.sort()
+	if len(lengths) != 3:
+		raise ValueError('Wron number of entries in a line')
+	if sum(lengths[:-1]) > lengths[-1]:
+		count_good += 1
+	count_total += 1
+
+print("Found {0} triangles, and {1} are possible".format(count_total,count_good))
