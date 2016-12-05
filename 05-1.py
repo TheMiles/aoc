2d0b09030b0fbd90bdab41f905c8bb2b1024f927
Miles <miles@coders-of-the-caribbean.org>
2016-12-06 00:34:31 +0100

    Dec 5th second problem

--- 05-1.py
+++ 05-1.py
@@ -10,15 +10,21 @@ parser.add_argument('file', type=argparse.FileType('r'),
 args = parser.parse_args()
 
 for l in [ x.strip() for x in args.file]:
-	password = ''
+	password = '--------'
 	i = -1
-	while len(password) < 8:
+	while '-' in password:
 		i += 1
 		data = (l + str(i)).encode('utf-8')
 		m = hashlib.md5(data)
 		d = m.hexdigest()
+
+		if i % 1000 == 0:
+			print(" Password {0}  {1}".format(password,i), end='\r')
+
 		if d[:5] == '00000':
-			password += d[5]
-			print("Found a letter '{0}' at index {1}, password so far {2}".format(d[5],i,password))
+			if ord(d[5]) >= ord('0') and ord(d[5]) < ord('8'):
+				position = int(d[5])
+				if password[position] == '-':
+					password = password[:position] + d[6] + password[position+1:]
 	
-	print("Password for {0} is: {1}".format(l, password))
+	print(" Password {0}".format(password))
