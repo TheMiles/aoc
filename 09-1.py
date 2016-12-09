86109789abedebdf4af73e10225c160688a64ea3
Michael Wagner <Michael_Wagner@mentor.com>
2016-12-09 14:09:12 +0100

    Working

--- 09-1.py
+++ 09-1.py
@@ -9,6 +9,40 @@ parser.add_argument('file', type=argparse.FileType('r'),
 
 args = parser.parse_args()
 
+count_length = 0
+
+def read_marker(l,i):
+    data   = []
+    select = 0
+    while l[i] != ')':
+        c = l[i]
+        if c == '(':
+            data.append('')
+        elif c == 'x':
+            data.append('')
+            select += 1
+        else:
+            data[select] += c
+        i += 1
+    return i+1, [int(x) for x in data]
+
+
 for l in [ x.strip() for x in args.file]:
-    print(l)
+    out = ''
+    i = 0
+    while i < len(l):
+        c = l[i]
+
+        if l[i] == '(':
+            i, [length,repitions] = read_marker(l,i)
+            out += l[i:i+length] * repitions
+            i   += length
+        else:
+            out += l[i]
+            i+=1
+    count_length += len(out)
+    print(l,out, len(out))
+
+print(count_length)
+
 
