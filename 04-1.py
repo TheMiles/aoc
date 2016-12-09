764c81f6748aa035bb7aff9d8583e7a8a08eec72
Miles <miles@coders-of-the-caribbean.org>
2016-12-10 00:39:32 +0100

    04th day second problem working

--- 04-1.py
+++ 04-1.py
@@ -28,9 +28,12 @@ def is_real_room(counts, checksum):
 		if prev < c or c < at_least_that_often:
 			return False
 		prev = c
-
-
 	return True
+
+def rot_char(c,n):
+	if c == '-': return ' '
+	index = (ord(c) - ord('a') + n) % 26
+	return chr( ord('a') + index )
 
 sum_of_room_sectors = 0
 
@@ -45,7 +48,11 @@ for l in [ x.strip() for x in args.file]:
 	count    = count_occurences(letters)
 	is_real  = is_real_room(count,checksum)
 
-	# print(sectorID, letters, count, checksum, is_real)
-	sum_of_room_sectors += sectorID if is_real else 0
+	decoded  = str().join([rot_char(c,sectorID) for c in chiffre])
+
+	# print(sectorID, letters, count, checksum, is_real, decoded)
+	if is_real:
+		sum_of_room_sectors += sectorID
+		print(sectorID, decoded)
 
 print(sum_of_room_sectors)
