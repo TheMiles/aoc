cfba8a79e64245c0c723c862bf49fa2f13fc71a0
Michael Wagner <Michael_Wagner@mentor.com>
2017-12-06 17:40:04 +0100

    Day 4 part 1

---
+++ 04.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Advent of code')
+parser.add_argument('input', metavar='file', type=argparse.FileType('r'))
+
+def isInList(s,l):
+    for x in l:
+        if s == x: return True
+    return False
+
+
+args = parser.parse_args()
+
+countValid = 0
+lines = [ x.strip() for x in args.input.readlines() ]
+for l in lines:
+    words = l.split()
+
+    isValid = True
+    for i in range(len(words)):
+        isValid = isValid and not isInList(words[i],words[i+1:])
+    print("The passphrase '{0}' is {1}".format(l, "valid" if isValid else "INVALID"))
+    countValid += 1 if isValid else 0
+
+print("There are {0} valid passphrases".format(countValid))
