8b2784e42b3b0a73977e5009bd0d5e5a8a5c866c
Michael Wagner <Michael_Wagner@mentor.com>
2017-12-15 14:17:23 +0100

    Day 10 part 2

--- 10.py
+++ 10.py
@@ -5,37 +5,75 @@ import re
 from collections import defaultdict
 
 parser = argparse.ArgumentParser(description='Advent of code')
-parser.add_argument('size', type=int)
 parser.add_argument('input', metavar='file', type=argparse.FileType('r'))
+parser.add_argument('-s','--size', type=int, default=256)
+parser.add_argument('-r','--rounds', type=int, default=64)
 
 args = parser.parse_args()
 
+def getHex(v):
+    if type(v) is str or type(v) is list:
+        if len(v) > 1:
+            return getHex(v[0]) + getHex(v[1:])
+        v = v[0]
+    return hex(v)[2:].zfill(2)
+
+def getSparseHash(sparseLength, rounds, hashValues):
+
+    sparseHash  = list(range(sparseLength))
+    lengths = [ ord(x) for x in hashValues ] + [17, 31, 73, 47, 23]
+
+    i     = 0
+    skip  = 0
+    shift = 0
+
+    for r in range(rounds):
+        for l in lengths:
+            assert l <= len(sparseHash)
+
+            #rotate sparseHash according to i
+            sparseHash = sparseHash[i:] + sparseHash[:i]
+
+            #apply inversion
+            sparseHash = list(reversed(sparseHash[:l])) + sparseHash[l:]
+
+            #rotate back
+            sparseHash = sparseHash[-i:] + sparseHash[:-i]
+
+            #adjust i
+            i += l + skip
+            i %= len(sparseHash)
+            skip += 1
+    return sparseHash
 
 
 
+def densifyHash(v):
+    assert( len(v)%16 == 0 )
 
-circle  = list(range(args.size))
-lengths = [ int(x.strip()) for x in args.input.readline().split(',')]
+    sparseHash = v[:]
+    denseHash  = [0] * int(len(sparseHash)/16)
+    i = 0
 
-i     = 0
-skip  = 0
-shift = 0
+    while sparseHash:
+        block      = sparseHash[:16]
+        sparseHash = sparseHash[16:]
 
-for l in lengths:
-    assert l <= len(circle)
+        while block:
+            denseHash[i] = denseHash[i] ^ block[0]
+            block        = block[1:]
 
-    #rotate circle according to i
-    circle = circle[i:] + circle[:i]
+        i += 1
 
-    #apply inversion
-    circle = list(reversed(circle[:l])) + circle[l:]
+    return denseHash
 
-    #rotate back
-    circle = circle[-i:] + circle[:-i]
+lines = [ x.strip() for x in args.input.readlines() ]
 
-    #adjust i
-    i += l + skip
-    i %= len(circle)
-    skip += 1
+for line in lines:
 
-print("The product of the first ot elements is", circle[0] * circle[1])
+    sparseHash = getSparseHash(args.size, args.rounds, line)
+    denseHash  = densifyHash(sparseHash)
+
+    print(getHex(denseHash))
+
+# print("The product of the first ot elements is", circle[0] * circle[1])
