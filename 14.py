19ce4f9a1869259fe6575f78baa7e5a1aba7df40
Michael Wagner <Michael_Wagner@mentor.com>
2017-12-19 15:39:40 +0100

    Day 14 part 2

--- 14.py
+++ 14.py
@@ -19,14 +19,43 @@ def hexToBin(v):
     num_of_bits = 4 * len(v)
     return str(bin(int(v, 16))[2:].zfill(num_of_bits))
 
+def fillFieldWithColor(rows,x,y, color):
+    if(x < 0): return
+    if(y < 0): return
+    if(y >= len(rows)): return
+    if(x >= len(rows[0])): return
+    if rows[y][x] != '#': return
+
+    rows[y][x] = color
+    fillFieldWithColor(rows, x-1, y,   color)
+    fillFieldWithColor(rows, x+1, y,   color)
+    fillFieldWithColor(rows, x,   y-1, color)
+    fillFieldWithColor(rows, x,   y+1, color)
+
+
+def colorizeField(rows):
+    maxNumberOfColors = 0
+    for i,r in enumerate(rows):
+        for j,v in enumerate(r):
+            if v == '#':
+                maxNumberOfColors += 1
+                fillFieldWithColor(rows,j,i,maxNumberOfColors)
+    return maxNumberOfColors
+
+
 if __name__ == '__main__':
     args = getArguments()
 
     lines = [ x.strip() for x in args.input.readlines() ]
 
     for line in lines:
-        countOnes = 0
+        rows = []
         for i in range(128):
             l = line + "-" + str(i)
-            countOnes += hexToBin(getHex(densifyHash(getSparseHash(args.size, args.rounds, l)))).count('1')
-        print(line,"has",countOnes,"ones")
+            rows.append(list(hexToBin(getHex(densifyHash(getSparseHash(args.size, args.rounds, l)))).replace('1','#').replace('0','.')))
+
+        numColors = colorizeField(rows)
+        for x in rows:
+            print(x)
+
+        print("There are", numColors, "fields")
