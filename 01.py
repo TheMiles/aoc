2761fd1340d615f71a6691bc1bcc9320fec6485c
Miles <miles@coders-of-the-caribbean.org>
2018-12-01 20:21:28 +0100

    Day 01 part 2

--- 01.py
+++ 01.py
@@ -7,16 +7,24 @@ parser.add_argument('input', metavar='file', type=argparse.FileType('r'))
 
 args = parser.parse_args()
 
-lines = [ x.strip() for x in args.input.readlines() ]
-sum = 0
-for l in lines:
-    if l:
-        op = l[0]
-        n  = int(l[1:])
-        if op == '+':
-            sum += n
-        elif op == '-':
-            sum -= n
-        else:
-            print("ERROR")
+lines     = [ x.strip() for x in args.input.readlines() ]
+sum       = 0
+freqs     = set()
+searching = True
+
+while searching:
+    for l in lines:
+        if l:
+            op = l[0]
+            n  = int(l[1:])
+            if op == '+':
+                sum += n
+            elif op == '-':
+                sum -= n
+            else:
+                print("ERROR")
+            searching = not sum in freqs
+            freqs.add(sum)
+            if not searching:
+                break
 print("Result is {0}".format(sum))
