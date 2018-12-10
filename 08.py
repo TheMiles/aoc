53238183efc755c554c8c89eee2df0768e89849b
Miles <miles@coders-of-the-caribbean.org>
2018-12-10 08:50:59 +0100

    Day 8 part 1

--- 08.py
+++ 08.py
@@ -17,28 +17,44 @@ if __name__ == '__main__':
     lines        = list(filter(None, [ x.strip() for x in args.input.readlines() ] ))
     licenseFiles = [ [int(y) for y in x] for x in (l.split(' ') for l in lines) ]
 
-    print(licenseFiles)
     for license in licenseFiles:
 
         licenseSum     = 0
+        nodeValues     = []
         nodesToRead    = [1]
         metaDataToRead = []
+        numChildren    = []
         i              = 0
         while i < len(license):
 
-            print("i {0}: {1}  with nodesToRead {2} metaDataToRead {3} sum {4}".format(i,license[i],nodesToRead,metaDataToRead,licenseSum))
+            # print("i {0}: {1}  with nodesToRead {2} metaDataToRead {3} numChildren {5} nodeValues {6} sum {4}".format(i,license[i],nodesToRead,metaDataToRead,licenseSum, numChildren, nodeValues))
 
             if nodesToRead[-1] == 0:
                 numReadMetaData = metaDataToRead.pop()
+                nodeSum = 0
+                nc = numChildren[-1]
+
                 for m in range(numReadMetaData):
-                    licenseSum += license[i]
-                    i += 1
+                    md = license[i+m]
+                    licenseSum += md
+                    # print("{3} Metadata {0} nodeSum {1} nodeValues {2}".format(md, nodeSum, nodeValues, i+m))
+                    if nc == 0:
+                        nodeSum += md
+                    elif md <= nc:
+                        nodeSum += nodeValues[-nc+md-1]
+
+                i += numReadMetaData
+                if nc > 0:
+                    nodeValues = nodeValues[:-nc]
+                nodeValues.append(nodeSum)
+                numChildren.pop()
                 nodesToRead.pop()
                 continue
 
             nodesToRead[-1] -= 1
             nodesToRead.append(license[i])
+            numChildren.append(license[i])
             metaDataToRead.append(license[i+1])
             i += 2
 
-        print("The license sum is ", licenseSum)
+        print("The license sum is ", licenseSum, " and the value ", nodeValues[0])
