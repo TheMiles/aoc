cc4177101ef2364a020f67721e702c1d838d67d6
Miles <miles@coders-of-the-caribbean.org>
2019-12-03 09:00:10 +0100

    Copying from day 2 and adding the examples and input

---
+++ 03.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import argparse
+
+def getArguments():
+    parser = argparse.ArgumentParser(description='Advent of code')
+    parser.add_argument('input', metavar='file', type=argparse.FileType('r'))
+    parser.add_argument('-n','--noun', type=int, default=12)
+    parser.add_argument('-v','--verb', type=int, default=2)
+    parser.add_argument('-t','--target', type=int)
+
+    return parser.parse_args()
+
+
+def add(c):
+    m  = c[0]
+    pc = c[1]
+
+    a  = m[m[pc+1]]
+    b  = m[m[pc+2]]
+    m[m[pc+3]] = a + b
+
+    c[1] = pc+4
+
+def mul(c):
+    m  = c[0]
+    pc = c[1]
+
+    a  = m[m[pc+1]]
+    b  = m[m[pc+2]]
+    m[m[pc+3]] = a * b
+
+    c[1] = pc+4
+
+
+def hac(c):
+    c[1] = -1
+
+
+opcodes = {
+    1:  add,
+    2:  mul,
+    99: hac
+}
+
+
+def run(c):
+    while c[1] >= 0:
+        p  = c[0]
+        pc = c[1]
+        opcodes[p[pc]](c)
+
+
+
+if __name__ == '__main__':
+    args = getArguments()
+    lines = list(filter(None, [ [ int(y) for y in x.strip().split(',')] for x in args.input.readlines() ] ))
+
+    for p in lines:
+
+        print("Trying", p)
+
