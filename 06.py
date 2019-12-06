ed0100f7eb32dbc7519caf7164f9f374102fd2df
Miles <miles@coders-of-the-caribbean.org>
2019-12-06 22:17:23 +0100

    Day 06 part 1

---
+++ 06.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+import argparse
+
+def getArguments():
+    parser = argparse.ArgumentParser(description='Advent of code')
+    parser.add_argument('input', metavar='file', type=argparse.FileType('r'), nargs='?')
+    parser.add_argument('-d', '--direct', type=str)
+
+
+    return parser.parse_args()
+
+class Object(object):
+
+    def __init__(self, obj):
+        self.obj      = obj
+        self.parent   = None
+        self.children = []
+
+class Orbits(object):
+
+    def __init__(self):
+        self.objects = []
+        self.lookup  = {}
+
+    def getIndex(self, obj):
+        if not obj in self.lookup:
+            self.insertObj(obj)
+        return self.lookup[obj]
+
+    def insertObj(self, obj):
+        if obj in self.lookup: return
+        self.lookup[obj] = len(self.objects)
+        self.objects.append(Object(obj))
+        # print("Inserting", obj)
+
+    def addChild(self, parent, child):
+        parent_index = self.getIndex(parent)
+        child_index  = self.getIndex(child)
+
+        assert(self.objects[child_index].parent is None)
+
+        # print("Adding to {} child {}".format(self.objects[parent_index].obj, self.objects[child_index].obj))
+        self.objects[child_index].parent = parent_index
+        self.objects[parent_index].children.append(child_index)
+
+
+    def numberOfOrbitsForIndex(self, index):
+        num = -1
+        i   = index
+        while i is not None:
+            num += 1
+            i    = self.objects[i].parent
+        # print("Orbit number for {} is {} direct orbit of {}".format(self.objects[index].obj, num, self.objects[self.objects[index].parent].obj if self.objects[index].parent is not None else "NONE"))
+        return num
+
+    def totalNumberOfOrbits(self):
+        num = 0
+        for i in range(len(self.objects)):
+            num += self.numberOfOrbitsForIndex(i)
+        return num
+
+
+if __name__ == '__main__':
+    args = getArguments()
+    lines = list(filter(None, [ x.strip().split(')') for x in args.input.readlines() ] ))
+
+    o = Orbits()
+
+    for p in lines:
+        o.addChild(p[0],p[1])
+
+    print(o.totalNumberOfOrbits())
