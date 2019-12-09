f820bfe0b56aebe862dc7c9cc869d20bd7f9dd05
miles <miles@coders-of-the-caribbean.org>
2019-12-09 11:59:23 +0100

    Day 08 part 2

--- 08.py
+++ 08.py
@@ -2,6 +2,7 @@
 
 import argparse
 import numpy as np
+from PIL import Image
 
 def getArguments():
     parser = argparse.ArgumentParser(description='Advent of code')
@@ -10,6 +11,13 @@ def getArguments():
     parser.add_argument('-t','--height', type=int, default=6)
 
     return parser.parse_args()
+
+
+def compositeImages(a,b):
+    chooser = lambda x,y: y if x == 2 else x
+    c = np.array([chooser(d[0],d[1]) for d in zip(a.flatten(),b.flatten())])
+    c = c.reshape(a.shape)
+    return c
 
 
 if __name__ == '__main__':
@@ -28,9 +36,16 @@ if __name__ == '__main__':
         zeros= args.width*args.height
         sum  = 0
 
-        for c in counts:
-            # print("Layer",c[0] if 0 in c else 0, c[1], c[2], c[1]*c[2], c)
-            if 0 in c and c[0]< zeros:
-                sum   = c[1]*c[2]
-                zeros = c[0]
-        print("Answer:",sum)
+        # for c in counts:
+        #     # print("Layer",c[0] if 0 in c else 0, c[1], c[2], c[1]*c[2], c)
+        #     if 0 in c and c[0]< zeros:
+        #         sum   = c[1]*c[2]
+        #         zeros = c[0]
+        # print("Answer:",sum)
+
+        composite = image[0]
+        for layer in image[1:]:
+            composite = compositeImages(composite,layer)
+
+        im = Image.fromarray(np.uint8(composite*255),'L')
+        im.show()
