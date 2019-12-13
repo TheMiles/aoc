b1702957df2558170c93e8e22094ef023705d999
miles <miles@coders-of-the-caribbean.org>
2019-12-13 13:36:49 +0100

    Day 12 part 2

--- 12.py
+++ 12.py
@@ -3,6 +3,7 @@
 import argparse
 import numpy as np
 import re
+from datetime import datetime
 
 
 def getArguments():
@@ -13,6 +14,35 @@ def getArguments():
     return parser.parse_args()
 
 
+class Progress(object):
+
+    def __init__(self, number):
+        self.number = number
+        self.count  =  0
+        self.prev   = -1
+        self.start  = None
+
+    def reset(self):
+        self.count  = 0
+        self.prev   = -1
+        self.start  = None
+
+    def __str__(self):
+        percent    = self.percent()
+        dt         = datetime.now() - self.start
+        total_time = dt * 100 / percent if percent > 0 else -1
+        return "{:.2f}% remaining time {} of  {}".format(self.percent(), (total_time-dt), total_time)
+
+    def percent(self):
+        return 100*self.count/self.number
+
+    def tick(self):
+        if self.start is None:  self.start = datetime.now()
+        self.count += 1
+        percent = int(self.percent())
+        if percent != self.prev:
+            self.prev = percent
+            print(self)
 
 
 class Moon(object):
@@ -53,6 +83,28 @@ def iterateSystem(moons):
         m.move()
 
 
+def createFastMoon(moons, axis):
+    return np.array([[m.pos[axis],m.vel[axis]] for m in moons])
+
+
+def applyForceFastMoon(fastMoons):
+    fastMoons[:,1] += np.array(list(map(lambda x: np.sum(np.sign(fastMoons[:,0]-x)), fastMoons[:,0])))
+    fastMoons[:,0] += fastMoons[:,1]
+
+def findCycleLengthFastMoon(moons, axis):
+    fastMoons  = createFastMoon(moons,axis)
+    firstMoon  = fastMoons.copy()
+
+    i = 0
+    while True:
+        i += 1
+        applyForceFastMoon(fastMoons)
+        if np.array_equal(firstMoon, fastMoons):
+            break
+    return i
+
+
+
 positionPattern = re.compile(r"<x=(-?\d+), y=(-?\d+), z=(-?\d+)>")
 
 if __name__ == '__main__':
@@ -64,10 +116,11 @@ if __name__ == '__main__':
         m = positionPattern.match(p)
         moons.append(Moon([int(m[1]),int(m[2]),int(m[3])]))
 
-    for i in range(args.iterations):
-        print(i,[str(m) for m in  moons])
-        iterateSystem(moons)
+    iterations = [0,0,0]
+    iterations[0] = findCycleLengthFastMoon(moons,0)
+    iterations[1] = findCycleLengthFastMoon(moons,1)
+    iterations[2] = findCycleLengthFastMoon(moons,2)
 
-    print(args.iterations,[str(m) for m in  moons])
-    print("Energy", sum([m.energy() for m in moons]))
+    lcm = np.lcm.reduce(iterations)
 
+    print(iterations, lcm)
