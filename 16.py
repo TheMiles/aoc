90b6f82e8c3dc4918a71d25faa7e718a879fd826
Miles <miles@coders-of-the-caribbean.org>
2019-12-16 08:41:36 +0100

    Stubs for Day 16

---
+++ 16.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import argparse
+
+def getArguments():
+    parser = argparse.ArgumentParser(description='Advent of code')
+    parser.add_argument('input', metavar='file', type=argparse.FileType('r'))
+    return parser.parse_args()
+
+def main():
+
+if __name__ == '__main__':
+    main()
