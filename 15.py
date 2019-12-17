ba0d1f67af1febf817460064d4b55d4c7999c356
miles <miles@coders-of-the-caribbean.org>
2019-12-17 11:49:25 +0100

    Moving content to sparate utils folder

--- 15.py
+++ 15.py
@@ -5,6 +5,8 @@ import intcode as ic
 from collections import defaultdict
 import numpy as np
 import curses
+from utils.getch import getch
+from utils.fields import Field
 
 
 def getArguments():
@@ -13,150 +15,19 @@ def getArguments():
 
     return parser.parse_args()
 
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
 
-    def __call__(self): return self.impl()
-
-
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
-
-
-getch = _Getch()
-
-
-class Field(object):
+class ScreenField(Field):
 
     def __init__(self, screen):
-        self.content = {
-            0: ' ',
-            1: '#',
-            2: '.',
-            3: 'O'
-        }
-
-        self.c      = np.array([[2]])
-        self.offset = np.array([0,0])
-        self.robot  = np.array([0,0])
+        Field.__init__(self)
         self.screen = screen
 
 
-    def setRobot(self, pos):
-        self.robot = pos+self.offset
-
     def printField(self):
-        for y in range(self.c.shape[0]):
-            for x in range(self.c.shape[1]):
-                p = np.array([y,x])
-                if np.equal(self.robot, p).all():
-                    self.screen.addstr(y,x,'R')
-                else:
-                    self.screen.addstr(y,x,self.getContent(p-self.offset))
+        l = self.getStringList()
+        for y,r in enumerate(l):
+            self.screen.addstr(y,0,"".join(r))
         self.screen.refresh()
-
-    def getAdjacentContent(self, pos):
-        adj = [
-            np.array([-1, 0]),
-            np.array([ 1, 0]),
-            np.array([ 0,-1]),
-            np.array([ 0, 1])
-        ]
-
-        result = []
-        for a in adj:
-            nextPos = pos+a
-            result.append((nextPos, self.getContent(nextPos)))
-
-        return result
-
-    def countContent(self, content):
-        c = 0
-        for key, value in self.content.items():
-            if value == content:
-                c = key
-
-        return len(list(filter(lambda x:c==x, self.c.flatten())))
-
-
-    def getContent(self, pos):
-        return self.content[self.getValue(pos)]
-
-    def setContent(self, pos, content):
-        for key, value in self.content.items():
-            if value == content:
-                self.setValue(pos,key)
-                return
-
-
-    def getValue(self, pos):
-        p = pos+self.offset
-        if np.less(p,0).any() or np.greater_equal(p,self.c.shape).any(): return 0
-        return self.c[tuple(p)]
-
-    def setValue(self, pos, value):
-        p = pos+self.offset
-
-        # print("setValue",p,pos,self.offset,self.c.shape)
-        if np.less(p,0).any() or np.greater_equal(p,self.c.shape).any:
-            self.growField(p)
-            p = pos+self.offset
-            # print("afterGrow",p,pos,self.offset,self.c.shape)
-
-        self.c[tuple(p)] = value
-        # print("field now",self.c)
-
-    def growField(self,pos):
-
-        height, width = self.c.shape
-        if pos[1]<0:
-            d = abs(pos[1])
-            self.c = np.concatenate((np.zeros(d*height,dtype=np.uint8).reshape(height,d),self.c),axis=1)
-            width += d
-            self.offset[1] += d
-            self.robot[1] += d
-        elif pos[1]>=width:
-            d = pos[1]-width+1
-            self.c = np.concatenate((self.c,np.zeros(d*height,dtype=np.uint8).reshape(height,d)),axis=1)
-            width += d
-
-        if pos[0]<0:
-            d = abs(pos[0])
-            self.c = np.concatenate((np.zeros(d*width,dtype=np.uint8).reshape(d,width),self.c),axis=0)
-            height += d
-            self.offset[0] += d
-            self.robot[0] += d
-        elif pos[0]>=height:
-            d = pos[0]-height+1
-            self.c = np.concatenate((self.c,np.zeros(d*width,dtype=np.uint8).reshape(d,width)),axis=0)
-            height += d
-
 
 
 class Robot(object):
@@ -289,7 +160,7 @@ def main(stdscr):
 
     for p in lines:
 
-        field = Field(stdscr)
+        field = ScreenField(stdscr)
         robot = Robot(p,field)
         robot.run()
 
---
+++ utils/fields.py
@@ -0,0 +1,114 @@
+import numpy as np
+
+
+
+class Field(object):
+
+    def __init__(self):
+        self.content = {
+            0: ' ',
+            1: '#',
+            2: '.',
+            3: 'O'
+        }
+
+        self.c      = np.array([[2]])
+        self.offset = np.array([0,0])
+        self.robot  = np.array([0,0])
+
+    def setRobot(self, pos):
+        self.robot = pos+self.offset
+
+    def getStringList(self):
+        l = []
+        for y in range(self.c.shape[0]):
+            c = []
+            for x in range(self.c.shape[1]):
+                p = np.array([y,x])
+                if np.equal(self.robot, p).all():
+                    c.append('R')
+                else:
+                    c.append(self.getContent(p-self.offset))
+            l.append(c)
+        return l
+
+    def getAdjacentContent(self, pos):
+        adj = [
+            np.array([-1, 0]),
+            np.array([ 1, 0]),
+            np.array([ 0,-1]),
+            np.array([ 0, 1])
+        ]
+
+        result = []
+        for a in adj:
+            nextPos = pos+a
+            result.append((nextPos, self.getContent(nextPos)))
+
+        return result
+
+    def countContent(self, content):
+        c = 0
+        for key, value in self.content.items():
+            if value == content:
+                c = key
+
+        return len(list(filter(lambda x:c==x, self.c.flatten())))
+
+
+    def getContent(self, pos):
+        return self.content[self.getValue(pos)]
+
+    def setContent(self, pos, content):
+        for key, value in self.content.items():
+            if value == content:
+                self.setValue(pos,key)
+                return
+
+
+    def getValue(self, pos):
+        p = pos+self.offset
+        if np.less(p,0).any() or np.greater_equal(p,self.c.shape).any(): return 0
+        return self.c[tuple(p)]
+
+    def setValue(self, pos, value):
+        p = pos+self.offset
+
+        # print("setValue",p,pos,self.offset,self.c.shape)
+        if np.less(p,0).any() or np.greater_equal(p,self.c.shape).any:
+            self.growField(p)
+            p = pos+self.offset
+            # print("afterGrow",p,pos,self.offset,self.c.shape)
+
+        self.c[tuple(p)] = value
+        # print("field now",self.c)
+
+    def growField(self,pos):
+
+        height, width = self.c.shape
+        if pos[1]<0:
+            d = abs(pos[1])
+            self.c = np.concatenate((np.zeros(d*height,dtype=np.uint8).reshape(height,d),self.c),axis=1)
+            width += d
+            self.offset[1] += d
+            self.robot[1] += d
+        elif pos[1]>=width:
+            d = pos[1]-width+1
+            self.c = np.concatenate((self.c,np.zeros(d*height,dtype=np.uint8).reshape(height,d)),axis=1)
+            width += d
+
+        if pos[0]<0:
+            d = abs(pos[0])
+            self.c = np.concatenate((np.zeros(d*width,dtype=np.uint8).reshape(d,width),self.c),axis=0)
+            height += d
+            self.offset[0] += d
+            self.robot[0] += d
+        elif pos[0]>=height:
+            d = pos[0]-height+1
+            self.c = np.concatenate((self.c,np.zeros(d*width,dtype=np.uint8).reshape(d,width)),axis=0)
+            height += d
+
+
+class ContentField(Field):
+        def __init__(self,content):
+            pass
